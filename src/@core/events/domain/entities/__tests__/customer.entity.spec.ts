bfbeec4fb6bf770ea7fefcca96b04b163f2ddea7
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2023-07-22 13:16:09 -0300

    feat: entity equals

--- src/@core/events/domain/entities/__tests__/customer.entity.spec.ts
+++ src/@core/events/domain/entities/__tests__/customer.entity.spec.ts
@@ -1,18 +1,25 @@
 import { Customer, CustomerId } from '../customer.entity';
+import Cpf from '../../../../common/domain/value-objects/cpf.vo';
 
 test('should create customer', () => {
-  let customer = Customer.create({
+  const customer = Customer.create({
     name: 'Any',
     cpf: '049.192.120-97'
   })
 
-  console.log(customer)
   expect(customer).toBeInstanceOf(Customer)
   expect(customer.id).toBeDefined()
   expect(customer.id).toBeInstanceOf(CustomerId)
   expect(customer.name).toBe('Any')
   expect(customer.cpf.value).toBe('04919212097')
 
+  const customer2 = new Customer({
+    id: new CustomerId(customer.id.value),
+    name: 'Any',
+    cpf: new Cpf('93863219040')
+  })
+  console.log(customer.equals(customer2))
+  
   //  customer = new Customer({
   //    id: '123',
   //   name: 'Any',
