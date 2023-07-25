31fc01f0b8b3a044c1bd8347d4d427c8342dfd76
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2023-07-24 21:44:01 -0300

    feat: added mikro ORM PartnerId

---
+++ src/@core/events/infra/db/schemas.spec.ts
@@ -0,0 +1,29 @@
+import { MySqlDriver } from '@mikro-orm/mysql';
+import { MikroORM } from '@mikro-orm/mysql';
+import { PartnerSchema } from './schemas';
+import { Partner } from '../../domain/entities/partner.entity';
+
+test('should create partner', async () => {
+  const orm = await MikroORM.init<MySqlDriver>({
+    entities: [PartnerSchema],
+    dbName: 'events',
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    type: 'mysql',
+    forceEntityConstructor: true,
+  });
+  await orm.schema.refreshDatabase();
+  const em = orm.em.fork();
+  
+  const partner = Partner.create({ name: 'P1' })
+  em.persist(partner)
+  await em.flush()
+  await em.clear()
+  
+  const partnerFound = await em.findOne(Partner, { id: partner.id })
+  console.log(partnerFound)
+  
+  await orm.close()
+})
